508a688faa5c5716b64abd5c78293f993104f266
Antonio Netto <antonionettodev@gmail.com>
2026-01-15 01:47:28 -0300

    removed unused migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,1 @@
-import * as migration_20260113_180642 from './20260113_180642';
-
-export const migrations = [
-  {
-    up: migration_20260113_180642.up,
-    down: migration_20260113_180642.down,
-    name: '20260113_180642'
-  },
-];
+export const migrations = [{}]
